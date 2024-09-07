cdfbdd7cd40d3a373950883f1b8a2ed34273b427
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-09-07 13:04:02 +0530

    converted card list class component to functional component.

--- src/components/cardList/cardList.component.js
+++ src/components/cardList/cardList.component.js
@@ -1,17 +1,15 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import './card-list.styles.css';
 import CardContainer from './cardContainer/cardContainer.component';
 
-class CardList extends Component {
-    render() {
-        return (
-            <div className='card-list'>
-                {
-                    this.props.monsters.map(monster => <CardContainer monster={monster} />)
-                }
-            </div>
-        );
-    }
+const CardList = ({ monsters }) => {
+    return (
+        <div className='card-list'>
+            {
+                monsters.map(monster => <CardContainer key={monster.id} monster={monster} />)
+            }
+        </div>
+    );
 }
 
 export default CardList;
